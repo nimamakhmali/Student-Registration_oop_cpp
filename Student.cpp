e8d00ce315c46fbe4092533712a4b9fca13e0ba2
nimaMakhmali <nmkhmly790@gmail.com>
2024-03-27 19:14:22 +0330

    course

--- Student.cpp
+++ Student.cpp
@@ -1,8 +1,23 @@
+//*************************************************************
+//     Nima Makhmali ----->  Buali sina university of IRAN❤️
+//*************************************************************
 #include <iostream>
 #include <fstream>
 
-class Course
-{};
+using namespace std;
 
-class Registration
-{};
+const unsigned CnameSize=10;
+const unsigned MaxCourses=10;
+
+class Course
+{
+ private:
+  char name[CnameSize+1];
+  char section;
+  unsigned credits;
+
+  public:
+   Course();
+   void input(ifstream & infile);
+   void output(ofstream & ofile) const;
+};
